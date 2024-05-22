05d4714a8600900a9679a4bb73ea07bc5e29c9c0
Dani Chagas <chagasdaniel788@gmail.com>
2024-05-22 16:25:05 -0300

    criação dos atributos e entrada de dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        
+
+        //TODO: Adicionado a classe Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //TODO: Criando os atributos
+        int number;
+        String agency;
+        String customerName;
+        double balance;
+
+        //TODO: entrada de dados
+        System.out.println("Obrigado por escolher a gente para criarmos uma conta para você!");
     }
 }
